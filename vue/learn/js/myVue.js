574b6e692b7ac3521a92135c868ff401b186942a
tsubasa-syaoran <1412360581@qq.com>
2024-07-21 16:57:15 +0800

    2024/07/21 vue learn

--- vue/learn/js/myVue.js
+++ vue/learn/js/myVue.js
@@ -8,19 +8,22 @@ class Vue {
             let firstChar = propertyName.charAt(0)
             if(firstChar != '_' && firstChar != '$'){
                 Object.defineProperty(this,propertyName,{
+                    // 数据代理
                     get: function(){
                         return options.data[propertyName]
                     },
+                    // 数据劫持
                     set: function(val){
-                        this.options.data[propertyName] = val
+                        options.data[propertyName] = val
+                        console.log('页面上的DOM 元素更新了')
                     }
                 })
             }
          
         })
 
-        Object.keys(options.methods).forEach((methodName, index) => {
-             this[methodName] = options.methods[methodName]
-        })
+        // Object.keys(options.methods).forEach((methodName, index) => {
+        //      this[methodName] = options.methods[methodName]
+        // })
     }
 }
