d61a26f19861d32feac065182c483c62c1d05e88
tsubasa-syaoran <1412360581@qq.com>
2024-07-28 22:52:42 +0800

    2024/07/28 vue learn

---
+++ vue/vue_pro/src/components/Car2.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h3>品牌: {{ brand }}</h3>
+    <h3>价格: {{ cprice }}</h3>
+    <h3>颜色: {{ color }}</h3>
+    <button @click="add">价格加1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Car", // 组件名称，可选但推荐
+  data(){
+      return {
+        cprice : this.price
+      }
+  },
+  methods:{
+    add(){
+      console.log('heloo')
+      this.cprice++
+    }
+  },
+//   data() {
+//     return {
+//       brand: "宝马520",
+//       price: 10,
+//       color: "黑色",
+//     };
+//   },
+//    props: ['brand','color','price']
+
+// props:[
+//     brand: String,
+//     color: String,
+//     price: Number
+// ]
+    props:{
+      brand:{
+            type: String,
+            required: true
+        },
+        color:{
+            type: String,
+            default: '红色'
+        },
+        price:{
+            type: Number,
+            required: true
+        },
+    }
+}
+</script>
---
+++ vue/vue_pro/src/components/test.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h3>品牌: {{ brand }}</h3>
+    <h3>价格: {{ price }}</h3>
+    <h3>颜色: {{ color }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Car", // 组件名称，可选但推荐
+//   data() {
+//     return {
+//       brand: "宝马520",
+//       price: 10,
+//       color: "黑色",
+//     };
+//   },
+//    props: ['brand','color','price']
+
+// props:[
+//     brand: String,
+//     color: String,
+//     price: Number
+// ]
+props:[
+    brand:{
+        type: String,
+        required: true
+    },
+     color:{
+        type: String,
+        default: '红色'
+    },
+     price:{
+        type: Number,
+        required: true
+    },
+]
+}
+</script>
